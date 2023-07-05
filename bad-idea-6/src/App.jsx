d85c83b36e14c702c981330567f7441d6be3d0e5
agustin <agustinzamorajr@Agustins-MacBook-Pro.local>
2023-07-05 11:40:51 -0500

    added register.jsx

--- bad-idea-6/src/App.jsx
+++ bad-idea-6/src/App.jsx
@@ -4,7 +4,8 @@ import Home from './components/Home';
 import PostReview from "./components/PostReview";
 import AllReviews from "./components/AllReviews";
 import SingleReview from "./components/SingleReview";
-import Login from "./components/Login"
+import Login from "./components/Login";
+import Register from "./components/register";
 
 function App() {
 
@@ -17,13 +18,16 @@ function App() {
       <div id ="navbar">
         <Link to="/"> Home </Link>
         <Link to="/AllReviews"> All Reviews </Link>
+        <Link to="/loginPage"> Login</Link>
+        <Link to="/register"> Register</Link>
       </div>
       
       <div id="main-section">
         <Routes>
           <Route path="/newPost" element={<PostReview />} />
-          <Route path="/loginPage" element={<Login/>} />
+          <Route path="/loginPage" element={<Login />} />
           <Route path="/" element={<AllReviews />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </div>
---
+++ bad-idea-6/src/components/register.jsx
@@ -0,0 +1,90 @@
+import React, { useState} from "react";
+// import { registerUser } from ""//Update register User!!!!!!
+import { useNavigate } from "react-router-dom"
+import BASEURL from "./apiAdapters";
+
+const Register = (props) => {
+    const setIsLoggedIn = props.setIsLoggedIn
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [eMail, setEMail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate= useNavigate()
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+
+        try {
+            const result = await registerUser(username, password)
+            console.log(result)
+
+            localStorage.setItem('token', result.token)
+            setIsLoggedIn(true)
+
+            navigate('/')
+        }   catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+            <label>
+                    First Name:
+                    <input
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => {
+                            setFirstName(e.target.value);
+                    }}
+                />
+                </label>
+                <label>
+                    Last Name:
+                    <input
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => {
+                            setLastName(e.target.value);
+                    }}
+                />
+                </label>
+                <label>
+                    E-Mail:
+                    <input
+                        type="text"
+                        value={eMail}
+                        onChange={(e) => {
+                            setEMail(e.target.value);
+                    }}
+                />
+                </label>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                    }}
+                />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
