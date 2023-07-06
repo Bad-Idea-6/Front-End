4ed8d412bcfbe08585c233ed90f61901bb159865
Parker-Pond <parker.a.pond@gmail.com>
2023-07-06 12:57:58 -0600

    got profile page to show info from the user

--- bad-idea-6/src/components/Profile.jsx
+++ bad-idea-6/src/components/Profile.jsx
@@ -1,38 +1,44 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import {BASEURL, currentToken} from "./apiAdapters";
+import { useState, useEffect } from "react";
+import { BASEURL, currentToken } from "./apiAdapters";
 import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
-const [userLogin, setUser] = useState({})
-useEffect( ()=>{async function getMyGoshDarnInfo(){
-  try {
-    console.log("skdhgfiwsebgiueb")
-    const response = await fetch(`${BASEURL}/user/profile`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${currentToken}`
-      },
-      body: JSON.stringify({
-        id: localStorage.getItem("id")
-    })
-  
-    });
-    console.log("got to the json")
-    const result = await response.json();
-    console.log(result)
-    setUser(result)
-  } catch (error) {
-    console.log("profile ERROR", error)
-  }}
-  getMyGoshDarnInfo()
-  },[])
-  
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    async function getMyGoshDarnInfo() {
+      try {
+        console.log("fired use effect");
+        const response = await fetch(`${BASEURL}/user/profile`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${currentToken}`,
+          },
+          body: JSON.stringify({
+            id: localStorage.getItem("id"),
+          }),
+        });
+        console.log("got to the json");
+        const result = await response.json();
+        console.log(result);
+        setUser(result);
+        // console.log(user.)
+      } catch (error) {
+        console.log("profile ERROR", error);
+      }
+    }
+    getMyGoshDarnInfo();
+  }, []);
+
   return (
-      <div className="Profile-Container">
-        
-        <img src="https://sjohart.files.wordpress.com/2013/07/good-idea-bad-idea.jpg"></img>
-      </div>
-    );
-  }
+    <div className="Profile-Container">
+      <h1>
+        welcome {user.firstName} {user.lastName}
+      </h1>
+      <h3>username {user.username}</h3>
+
+      <img src="https://sjohart.files.wordpress.com/2013/07/good-idea-bad-idea.jpg"></img>
+    </div>
+  );
+}
