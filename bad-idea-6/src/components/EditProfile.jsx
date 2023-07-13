5890a46f93002a91dc7fcf58230acd07b51520b3
agustin <agustinzamorajr@Agustins-MacBook-Pro.local>
2023-07-13 12:56:30 -0500

    added try if else

--- bad-idea-6/src/components/EditProfile.jsx
+++ bad-idea-6/src/components/EditProfile.jsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { BASEURL } from "./apiAdapters";
-
 
 const EditProfile = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Call the RegisterUser function here passing the email and password values
-    RegisterUser(password, email)
-      .then((data) => {
-        console.log(data);
-        // Handle the result or redirect to a success page
-      })
-      .catch((error) => {
-        console.log(error);
-        // Handle the error or display an error message
+
+    try {
+      const response = await fetch(`${BASEURL}/user/editProfile`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
       });
+
+      if (response.ok) {
+        // Handle success
+        console.log("Profile updated successfully");
+      } else {
+        // Handle error
+        console.log("Error updating profile");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,9 +42,7 @@ const EditProfile = () => {
           <input
             type="text"
             value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label>
@@ -39,9 +50,7 @@ const EditProfile = () => {
           <input
             type="password"
             value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <button type="submit">Submit</button>
