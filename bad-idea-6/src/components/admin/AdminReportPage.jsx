929447e22d8fc99d2cc438061f36f740168513f1
Parker-Pond <parker.a.pond@gmail.com>
2023-07-16 05:19:40 -0600

    admin functionality added i did alot but i am to lazy to list it all

---
+++ bad-idea-6/src/components/admin/AdminReportPage.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import { BASEURL } from "../apiAdapters";
+import "./admin.css";
+import { currentToken } from "../apiAdapters";
+
+export default function AdminReportPage() {
+  const [allReview, setAllReview] = useState([]);
+  const [reset, setReset] = useState(1);
+
+  useEffect(() => {
+    async function fetchAllReviews() {
+      const APIURL = BASEURL;
+      try {
+        const response = await fetch(`${APIURL}/reviews`);
+        const translatedData = await response.json();
+        setAllReview(translatedData);
+      } catch (error) {
+        console.log("ERROR withFetch all reviews", error);
+      }
+    }
+    fetchAllReviews();
+  }, [reset]);
+
+  async function deleteReview(Id) {
+    try {
+      const response = await fetch(`${BASEURL}/delete/review`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${currentToken}`,
+        },
+        body: JSON.stringify({
+          reviewId: Id,
+        }),
+      });
+      const result = await response.json();
+      setReset(reset + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function resolveReport(Id) {
+    console.log("you pushed a button")
+    try {
+      const response = await fetch(`${BASEURL}/admin/resolve-report`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${currentToken}`,
+        },
+        body: JSON.stringify({
+          reviewId: Id,
+        }),
+      });
+      console.log("did this work?")
+      const result = await response.json();
+      setReset(reset + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <>
+      <div>
+        <h1>Reported Posts</h1>
+        {allReview && allReview.length ? (
+          allReview.map((rev, idx) => {
+            // console.log(rev);
+
+            return (
+              <>
+                {rev.reported ? (
+                  <>
+                    <div id="review-report-container" key={idx}>
+                      <h3>{rev.author}</h3>
+                      <h3 id="textWrap">{rev.review}</h3>
+                      <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        deleteReview(rev.reviewId);
+                      }}>Delete</button>
+                      <button
+                      onClick={(e)=>{
+                        e.preventDefault
+                        resolveReport(rev.reviewId)
+                      }}
+                      >Resolve</button>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </>
+            );
+          })
+        ) : (
+          <h3>Loding...</h3>
+        )}
+      </div>
+    </>
+  );
+}
---
+++ bad-idea-6/src/components/admin/AdminUserPage.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import { BASEURL } from "../apiAdapters";
+import "./admin.css";
+
+const Admin = () => {
+  const [users, setUsers] = useState([]);
+  const [howMany, setHowMany] = useState(users.length);
+  const [reset, setReset] = useState(1);
+  // const [userInQuestion, setUserInQuestion] = useState("")
+  const adminAuth = localStorage.getItem("is_admin");
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    async function fetchAllUsers() {
+      console.log(howMany);
+      try {
+        const response = await fetch(`${BASEURL}/admin/all-users`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+        });
+        console.log("got to the json");
+        const result = await response.json();
+        setUsers(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchAllUsers();
+  }, [reset]);
+  console.log(adminAuth);
+
+  async function createAdmin(id) {
+    try {
+      const response = await fetch(`${BASEURL}/admin/make-admin`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          adminId: id,
+        }),
+      });
+      const result = await response.json();
+      setReset(reset + 1)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function deleteUser(userId) {
+    try {
+      const response = await fetch(`${BASEURL}/delete/user`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userInQuestionId: userId,
+        }),
+      });
+      const result = await response.json();
+      setUsers(result);
+      setReset(reset + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      {adminAuth ? (
+        <div>
+          <h1>Admin control panel</h1>
+          {users && users.length ? (
+            users.map((user, idx) => {
+              return (
+                <div className="admin_user_container" key={user.userId}>
+                  <h3>{user.firstName}</h3>
+                  <h3>{user.lastName}</h3>
+                  <h3>{user.username}</h3>
+                  <h3>{user.email}</h3>
+                  {user.is_admin ? (
+                    <div>
+                    <h3>Is Admin</h3>
+                    </div>
+                  ) : (
+                    <div>
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteUser(user.userId);
+                        }}
+                      >
+                        Delete
+                      </button>
+                      <button onClick={(e) => {
+                          e.preventDefault();
+                          createAdmin(user.userId);
+                        }}>
+                        Make Admin</button>
+                    </div>
+                  )}
+                </div>
+              );
+            })
+          ) : (
+            <div> </div>
+          )}
+        </div>
+      ) : (
+        <h1>You are not an admin</h1>
+      )}
+    </>
+  );
+};
+
+export default Admin;
